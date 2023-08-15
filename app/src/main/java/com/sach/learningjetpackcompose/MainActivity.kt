455ebde93bb5a58165a00497f426a860fdd08370
Nilesh Salunkhe <nileshhsalunkhe14@gmail.com>
2023-08-15 11:51:19 +0530

    Learn Text and Text Style

--- app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
@@ -3,13 +3,26 @@ package com.sach.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.GenericFontFamily
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.sach.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +50,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearningJetpackComposeTheme {
-        Greeting("Android")
+        LearnTextView()
+    }
+}
+
+@Composable
+fun LearnTextView() {
+    Column {
+        Text(
+            text = "Android ",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Normal,
+                color = Color.Cyan,
+                shadow = Shadow(color=Color.Red, offset = Offset(4f,4f), blurRadius = 5f),
+            ),
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = "Android Studio", style = MaterialTheme.typography.titleLarge)
+
     }
 }
