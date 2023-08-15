96bdb21038d235aae662cd1190e8df4c745fd482
Nilesh Salunkhe <nileshhsalunkhe14@gmail.com>
2023-08-15 13:24:27 +0530

    Modified properties.

--- app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.sach.learningjetpackcompose
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.vector.VectorProperty
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.font.GenericFontFamily
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sach.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
@@ -54,13 +71,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LearningJetpackComposeTheme {
-        LearnTextView()
+        LearnButton()
     }
 }
 
 @Composable
 fun LearnTextView() {
-    Column {
+    Row {
         Text(
             text = "Android ",
             style = TextStyle(
@@ -68,12 +85,51 @@ fun LearnTextView() {
                 fontWeight = FontWeight.Bold,
                 fontStyle = FontStyle.Normal,
                 color = Color.Cyan,
-                shadow = Shadow(color=Color.Red, offset = Offset(4f,4f), blurRadius = 5f),
+                shadow = Shadow(color = Color.Red, offset = Offset(4f, 4f), blurRadius = 5f),
             ),
             maxLines = 3,
-            overflow = TextOverflow.Ellipsis
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(2f)
         )
-        Text(text = "Android Studio", style = MaterialTheme.typography.titleLarge)
+        Text(
+            text = "Android Studio",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.weight(4f)
+        )
 
+    }
+}
+
+@Composable
+fun LearnButton() {
+    Column {
+        LearnTextView()
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .height(60.dp)
+                .border(width = 2.dp, colorResource(id = R.color.black), shape = RectangleShape)
+
+        ) {
+            Text(text = "Click Here")
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Button(
+            { },
+            modifier = Modifier
+                .fillMaxWidth(0.5f),
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.purple_200))
+        ) {
+            Text(
+                "Value", modifier = Modifier
+                    .padding(10.dp)
+                    .background(Color.Red)
+                    .clip(shape = RectangleShape)
+
+            )
+        }
     }
 }
