299f5fd0f849ead92e99523f98ea55e1233d2ba0
Sachin Shelar <sachrohile@gmail.com>
2023-08-15 11:21:03 +0530

    Added Row and Column sample

--- app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sach/learningjetpackcompose/MainActivity.kt
@@ -3,14 +3,23 @@ package com.sach.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sach.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
+import java.sql.RowId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +46,28 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:Nexus 5X", showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearningJetpackComposeTheme {
-        Greeting("Android")
+        RowAndColumnSample()
+    }
+}
+
+
+@Composable
+fun RowAndColumnSample(){
+// Row in compose
+    Row(modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Sachin123")
+        Text(text = "Nilesh")
+
+        Column(verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxHeight()) {
+            Text(text = "Shelar11111111")
+            Text(text = "Salunke")
+        }
     }
 }
